88878ebd906fc086bf2bf899fc2f23af68ce4d8f
hfaildeestevez <hfaildeestevez@danielcastelao.org>
2021-03-02 10:58:29 +0100

    Initial commit

---
+++ src/MisMetdoso.java
@@ -0,0 +1,11 @@
+public class MisMetdoso {
+    /**
+     *Metodo que muestra un saludo
+     *
+     * @param mensaje saludo que quiero mostrar
+     * @return el mensaje concatenado con un saludo
+     */
+    public static String saludo(String mensaje){
+    return "saludo"+mensaje;
+    }
+}
